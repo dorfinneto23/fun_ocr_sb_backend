38d77796c169d9c01e3623bf6a111d893b2176f9
tech dor <tech.aharoni@gmail.com>
2024-05-04 01:10:34 +0300

    adding connections strings variables and log

--- function_app.py
+++ function_app.py
@@ -8,10 +8,24 @@ import json # in order to use json
 import pyodbc #for sql connections 
 from azure.servicebus import ServiceBusClient, ServiceBusMessage # in order to use azure service bus 
 
+# Azure Blob Storage connection string
+connection_string_blob = os.environ.get('BlobStorageConnString')
+
+#Azure service bus connection string 
+connection_string_servicebus = os.environ.get('servicebusConnectionString')
+
+# Define connection details
+server = 'medicalanalysis-sqlserver.database.windows.net'
+database = 'medicalanalysis'
+username = os.environ.get('sql_username')
+password = os.environ.get('sql_password')
+driver= '{ODBC Driver 18 for SQL Server}'
+
+
 app = func.FunctionApp()
 
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="ocr",
                                connection="medicalanalysis_SERVICEBUS") 
 def sb_ocr_process(azservicebus: func.ServiceBusMessage):
-    logging.info('Python ServiceBus Queues trigger processed a message: %s',
-                azservicebus.get_body().decode('utf-8'))
+    message_data = azservicebus.get_body().decode('utf-8')
+    logging.info('Received messageesds: %s', message_data)

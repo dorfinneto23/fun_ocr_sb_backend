211bb49a3fcac3462dcf736d616ebdceb1d32681
tech dor <tech.aharoni@gmail.com>
2024-05-19 13:17:57 +0300

    adding update_documents_entity_field function in order to update doc on storage table

--- function_app.py
+++ function_app.py
@@ -10,6 +10,8 @@ from azure.servicebus import ServiceBusClient, ServiceBusMessage # in order to u
 from azure.ai.documentintelligence import DocumentIntelligenceClient
 from azure.ai.documentintelligence.models import ContentFormat, AnalyzeDocumentRequest
 from azure.core.credentials import AzureKeyCredential
+from azure.data.tables import TableServiceClient, TableClient, UpdateMode # in order to use azure storage table  
+from azure.core.exceptions import ResourceExistsError, ResourceNotFoundError # in order to use azure storage table  exceptions 
 
 # Azure Blob Storage connection string
 connection_string_blob = os.environ.get('BlobStorageConnString')
@@ -27,6 +29,43 @@ database = 'medicalanalysis'
 username = os.environ.get('sql_username')
 password = os.environ.get('sql_password')
 driver= '{ODBC Driver 18 for SQL Server}'
+
+
+# Update field on specific entity/ row in storage table 
+def update_documents_entity_field(table_name, partition_key, row_key, field_name, new_value):
+    """
+    Updates a specific field of an entity in an Azure Storage Table.
+
+    Parameters:
+    - account_name: str, the name of the Azure Storage account
+    - account_key: str, the key for the Azure Storage account
+    - table_name: str, the name of the table
+    - partition_key: str, the PartitionKey of the entity
+    - row_key: str, the RowKey of the entity
+    - field_name: str, the name of the field to update
+    - new_value: the new value to set for the field
+    """
+    try:
+        # Create a TableServiceClient using the connection string
+        table_service_client = TableServiceClient.from_connection_string(conn_str=connection_string_blob)
+
+        # Get a TableClient
+        table_client = table_service_client.get_table_client(table_name)
+
+        # Retrieve the entity
+        entity = table_client.get_entity(partition_key, row_key)
+
+        # Update the field
+        entity[field_name] = new_value
+
+        # Update the entity in the table
+        table_client.update_entity(entity, mode=UpdateMode.REPLACE)
+        logging.info(f"update_documents_entity_field:Entity updated successfully.")
+
+    except ResourceNotFoundError:
+        logging.info(f"The entity with PartitionKey '{partition_key}' and RowKey '{row_key}' was not found.")
+    except Exception as e:
+        logging.info(f"An error occurred: {e}")
 
 
 # Generic Function to update documents  in the 'documents' table
@@ -168,9 +207,8 @@ def sb_ocr_process(azservicebus: func.ServiceBusMessage):
     if ocr_result_dic["status"] == "success":
         logging.info(f"ocr completed")
         #preparing data for service bus 
-        doc_id_int = int(doc_id)
         data = { 
-                "doc_id" : doc_id_int, 
+                "doc_id" : doc_id, 
                 "filename" :new_filename,
                 "path" :ocr_result_dic["path"],
                 "url" :ocr_result_dic["bloburl"],
@@ -180,7 +218,8 @@ def sb_ocr_process(azservicebus: func.ServiceBusMessage):
             } 
         json_data = json.dumps(data)
         create_servicebus_event("contentanalysis", json_data)
-        update_documents_generic(doc_id,"status",2) #update status to 2 "ocr done"
+        #update_documents_generic(doc_id,"status",2) #update status to 2 "ocr done"
+        update_documents_entity_field("documents",caseid,doc_id,"status",2) #update status to 2 "ocr done"
         logging.info(f"the ocr page number is {pagenumber} out of {totalpages}")
         if pagenumber==totalpages: #check if the last file passed 
             update_case_generic(caseid,"status",5) #update case status to 6 "ocr done"
@@ -188,7 +227,8 @@ def sb_ocr_process(azservicebus: func.ServiceBusMessage):
     else:
         errorMesg = ocr_result_dic["Description"]
         logging.info(f"error:{errorMesg}")
-        update_documents_generic(doc_id,"status",3) #update document status to 2 "ocr failed"
+        #update_documents_generic(doc_id,"status",3) #update document status to 2 "ocr failed"
+        update_documents_entity_field("documents",caseid,doc_id,"status",3) #update document status to 2 "ocr failed"
         update_case_generic(caseid,"status",6) #update case status to 6 "ocr failed"
 
     
